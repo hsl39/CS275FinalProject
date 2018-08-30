0bbcf323b45e16fae5b7424f310793094f2cf6e9
DESKTOP-MJAHOEC\Harrison <hsl39@drexel.edu>
2018-08-30 14:30:59 -0400

    Comments for nick

--- USDAModule.js
+++ USDAModule.js
@@ -6,6 +6,8 @@ class USDAModule extends EventEmitter{
 	constructor(){
 		super();
 	}
+	
+	//Gets nutritional information based on the ndbno number
 	getInformation(number){
 		var ndbno = number;
 		var URL = 'https://api.nal.usda.gov/ndb/reports/?ndbno='+ndbno+'&type=b&format=json&api_key=lZIqzPiEXSImPbcdpu5tanygbkEIXWc7XHIDdQxO';
@@ -19,6 +21,7 @@ class USDAModule extends EventEmitter{
 		
 	}	
 	
+	//Gets the ndbno list
 	getNDBNO(descr, length){
 		var URL = 'https://api.nal.usda.gov/ndb/search/?format=json&q='+descr+'&sort=r&max='+length+'&offset=0&api_key=lZIqzPiEXSImPbcdpu5tanygbkEIXWc7XHIDdQxO'; 
 		request(URL, function(error,response,body){
