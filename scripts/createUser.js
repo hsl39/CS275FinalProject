262ba4b784e2ed6e17e472027c27f9d1c8bb0617
DESKTOP-MJAHOEC\Harrison <hsl39@drexel.edu>
2018-09-04 19:30:55 -0400

    script folder

---
+++ scripts/createUser.js
@@ -0,0 +1,61 @@
+
+function createUser(){
+	var uId = document.getElementById('user').value;
+	var exists = false;
+	
+	var URL = "http://localhost:8080/login?user=" + uId;
+	
+	if(uId.length > 10){
+		$("#output").html("Username contains too many characters. Please try again");
+	}
+	else{
+		$("#output").html("");
+		params = {
+			user: uId
+		};
+	
+		exists = false;
+		
+		$.ajax({
+				type: "GET",
+				url: URL,
+				dataType: "html",
+				success: function(msg){
+					if(msg != 'Success'){
+					//----------------------
+						URL = "http://localhost:8080/createUser?user=" + uId;
+						$.ajax({
+							type: "POST",
+							url: URL,
+							dataType: "html",
+							data: params,
+							success: function(msg){
+								if(msg == 'Success'){
+									$("#output").html("User Successfully Created");
+									toLogin();
+								}
+								else
+									$("#output").html("Invalid USERNAME, Please Try Again");
+							},
+							error: function(xhr, ajaxOptions, thownError){
+								//Outputs error if server doesn't respond
+								$("#output").html("ERROR");
+							}
+						});
+					//------------------------------------------	
+					}
+					else
+						$("#output").html("UserName already exists, please try again");
+				},
+				error: function(xhr, ajaxOptions, thownError){
+					//Outputs error if server doesn't respond
+					$("#output").html("ERROR");
+				}
+			});
+		
+		
+		
+			
+		
+	}
+}
