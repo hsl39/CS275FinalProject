ec22aeae566304daea3a672f09dd1e87c21635d1
DESKTOP-9212HQ6\Nicholas Simpkins <nls83@drexel.edu>
2018-09-04 19:35:22 -0400

    request Food scripts and index page

---
+++ requestFood.js
@@ -0,0 +1,45 @@
+function requestFood(){
+ 
+	var URL = "http://localhost:8080/foodList?input=";
+	
+	var input = String(document.getElementById('food').value);
+	URL += decodeURI(input);
+	
+	var num = document.getElementById('num').value;
+	URL +="&num=" + num;
+	
+	$.ajax({
+			type: "GET",
+			url: URL,
+			dataType: "html",
+			success: function(msg){
+				//Outputs the answer if the server responds
+				$("#output").html(msg);
+			},
+			error: function(xhr, ajaxOptions, thownError){
+				//Outputs error if server doesn't respond
+				$("#output").html("ERROR: ");
+			}
+		});
+}
+		
+function requestNutri(ndbno){
+ 
+	var URL = "http://localhost:8080/nutrients?food=" + ndbno;
+	
+	
+	
+	$.ajax({
+			type: "GET",
+			url: URL,
+			dataType: "html",
+			success: function(msg){
+				//Outputs the answer if the server responds
+				$("#output").html(msg);
+			},
+			error: function(xhr, ajaxOptions, thownError){
+				//Outputs error if server doesn't respond
+				$("#output").html("ERROR");
+			}
+		});
+ }
