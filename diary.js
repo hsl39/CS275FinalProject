463c30847cb57fb4f7fe3b806b0af6d685654564
DESKTOP-9212HQ6\Nicholas Simpkins <nls83@drexel.edu>
2018-09-04 20:10:24 -0400

    updated w/ credentials

--- diary.js
+++ diary.js
@@ -7,10 +7,10 @@ console.log("Diary module loaded...")
 //database details
 var mysql = require('mysql');
 var con = mysql.createConnection({
-    host: '',
-    user: '',
-    password: '',
-    database: ''
+    host: 'localhost',
+    user: 'root',
+    password: 'drexelcs275',
+    database: 'Trutrition'
 });
 
 con.connect(function(err) {
@@ -31,38 +31,38 @@ utils.inherits(Diary, EventEmitter);
 //renders html on index.html
 Tables.prototype.getTables = function() {
   // html for creating the card
-    var str = `
-  `;
+    var str = "<h2> Display a table</h2> <input type='button' onclick='describeTable()' value='Describe Tables'> <br><br> <div id='outputTables'></div><br>";
+	
     this.emit('getTables', str);
 }
 
 //to show data in the card
 Tables.prototype.describeTables = function(tbl_name) {
-    // var self = this;
-    // con.query('SELECT * FROM ' + tbl_name, function(err, rows, fields) {
-    //     if (err) {
-    //         console.log(err);
-    //         self.emit('describeTables', 'Error while processing query...');
-    //     } else {
-    //         var html_str = '<table border="1">';
-    //         html_str += '<tr>';
-    //         var headers = [];
-    //         for (i = 0; i < fields.length; i++) {
-    //             headers.push(fields[i].name);
-    //             html_str += '<th>' + fields[i].name + '</th>';
-    //         }
-    //         html_str += '</tr>';
-    //         for (i = 0; i < rows.length; i++) {
-    //             html_str += '<tr>';
-    //             for (j = 0; j < headers.length; j++) {
-    //                 html_str += '<td>' + rows[i][headers[j]] + '</td>';
-    //             }
-    //             html_str += '</tr>';
-    //         }
-    //         html_str += '</table>'
-    //         self.emit('describeTables', html_str);
-    //     }
-    // });
+     var self = this;
+     con.query('SELECT * FROM ' + tbl_name, function(err, rows, fields) {
+         if (err) {
+             console.log(err);
+             self.emit('describeTables', 'Error while processing query...');
+         } else {
+             var html_str = '<table border="1">';
+             html_str += '<tr>';
+             var headers = [];
+             for (i = 0; i < fields.length; i++) {
+                 headers.push(fields[i].name);
+                 html_str += '<th>' + fields[i].name + '</th>';
+             }
+             html_str += '</tr>';
+             for (i = 0; i < rows.length; i++) {
+                 html_str += '<tr>';
+                 for (j = 0; j < headers.length; j++) {
+                    html_str += '<td>' + rows[i][headers[j]] + '</td>';
+                 }
+                 html_str += '</tr>';
+             }
+             html_str += '</table>'
+             self.emit('describeTables', html_str);
+         }
+     });
 };
 
 module.exports = diary;
